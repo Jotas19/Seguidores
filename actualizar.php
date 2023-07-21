020ad847ee37825ac6818649c8a4c874627ff981
Over Rivera <Overrivera0305@gmail.com>
2023-07-20 20:30:07 -0500

    Hola, como estan?
    esto es una prueba nada  mas para ver si estoy conectado, perdon por ser nuevo.

---
+++ actualizar.php
@@ -0,0 +1,10 @@
+<?php
+
+$mensajeEstilo = 'font-size: 48px; color: #ff0000; text-align: center; padding: 20px; background-color: #ffff00; border: 3px solid #0000ff; border-radius: 10px; box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);';
+
+
+$mensaje = '¡Por que aqui no hay nada??? Para que es? !';
+
+
+echo '<div style="' . $mensajeEstilo . '">' . $mensaje . '</div>';
+?>
